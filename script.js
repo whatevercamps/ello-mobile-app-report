faadd5112b1467e05ec2547886cd15cd8c6604c2
David Bautista <dj.bautista10@uniandes.edu.co>
2020-05-23 01:58:40 -0500

    aklsdkad

--- script.js
+++ script.js
@@ -259,13 +259,13 @@ $("#revealconn2").click(() => {
 $("#bugthreadsArreglacionEnlace1").click(() => {
   $("#threadsSpace").removeClass("show");
 });
-setScenes();
-// setTimeout(function () {
-//   phone.className = "phone view_1 rotate";
-//   $("#staticBackdrop").modal("show");
-//   setTimeout(function () {
-//     document.getElementById("scenesButton").addEventListener("click", (evt) => {
-//       console.log("click", evt);
-//     });
-//   }, 2000);
-// }, 1000);
+// setScenes();
+setTimeout(function () {
+  phone.className = "phone view_1 rotate";
+  $("#staticBackdrop").modal("show");
+  setTimeout(function () {
+    document.getElementById("scenesButton").addEventListener("click", (evt) => {
+      console.log("click", evt);
+    });
+  }, 2000);
+}, 1000);
